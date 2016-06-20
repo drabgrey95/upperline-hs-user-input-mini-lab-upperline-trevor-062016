61af1a10af46d1c355b2bc6a3cf977b4c1503f26
Teddy Foley <tef77777@gmail.com>
2016-06-20 17:11:29 +0000

    Done.

---
+++ trip.rb
@@ -0,0 +1,10 @@
+puts "Where do you want to stay in New York City?"
+wherestay = gets
+puts "What place do you want to visit?"
+wherevisit = gets
+puts "Where do you want to eat while staying in New York City?"
+whereeat = gets
+puts "Where long do you want to stay in New York City?"
+howmanynights = gets
+
+puts "You will stay at the #{wherestay} for #{howmanynights} while in New York City. You will visit #{wherevisit} and eat at #{whereeat}. Have a good trip!"
